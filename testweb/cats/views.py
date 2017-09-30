d7497846477e57e4eed1f5013eb62ff890badd74
catemlitten <clitten@luc.edu>
2017-09-30 11:49:18 -0500

    more functionality added. page outlines.

--- testweb/cats/views.py
+++ testweb/cats/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from .models import adorable_kitten
-from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
 
 def index(request):
     all_kittens = adorable_kitten.objects.all()
-    template = loader.get_template("cats/index.html")
-    context = {
-        'all_kittens': all_kittens,
-
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'all_kittens': all_kittens}
+    return render(request, 'cats/index.html', context)
 
 
 def detail(request, kitten_id):
-    return HttpResponse("<h2>Details for : " + str(kitten_id) + "</h2>")
+    try:
+        kitten = adorable_kitten.objects.get(id=kitten_id)
+    except adorable_kitten.DoesNotExist:
+        raise Http404("No kitten here.")
+    return render(request, 'cats/detail.html', {'kitten': kitten})

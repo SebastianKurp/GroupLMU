a98090eaef1b86342aa7f3b2166c684c3c2cc907
catemlitten <clitten@luc.edu>
2017-09-30 19:18:46 -0500

    More functionality. Basic pages being built. Not done working tonight, but pushing during a lull when I haven't destroyed anything.

--- testweb/cats/admin.py
+++ testweb/cats/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import adorable_kitten
+from .models import adorable_kitten, human
 
 admin.site.register(adorable_kitten)
+admin.site.register(human)
 
--- testweb/cats/models.py
+++ testweb/cats/models.py
@@ -5,15 +5,19 @@ class adorable_kitten(models.Model):
     breed = models.CharField(max_length=150)
     height = models.FloatField(max_length=100)
     cat_name = models.CharField(max_length=200)
+    cat_pic = models.CharField(max_length=500)
 
     def __str__(self):
         return self.breed + ", " + self.cat_name + " is " + str(self.height) + " tall"
 
 
-class humans(models.Model):
+class human(models.Model):
     human_name = models.CharField(max_length=150)
-    height = models.FloatField
+    height = models.FloatField(max_length=100)
     cat_overlord = models.ForeignKey(adorable_kitten, on_delete=models.CASCADE)
+
     # if the cat is deleted, the human is unimportant
     # so delete it also
-
+    def __str__(self):
+        return str(self.human_name) + " is owned by a cat."
+        # cat_overlord returns an adorable_kitten object

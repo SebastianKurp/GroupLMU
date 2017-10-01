24c5d3690ab24f4e9042fcc4bfbf77e8917bbb79
catemlitten <clitten@luc.edu>
2017-09-30 20:55:32 -0500

    small additions to functionality. add/remove (t/f) favorite bool

--- testweb/cats/models.py
+++ testweb/cats/models.py
@@ -15,6 +15,7 @@ class human(models.Model):
     human_name = models.CharField(max_length=150)
     height = models.FloatField(max_length=100)
     cat_overlord = models.ForeignKey(adorable_kitten, on_delete=models.CASCADE)
+    is_favorite = models.BooleanField(default=False)
 
     # if the cat is deleted, the human is unimportant
     # so delete it also
--- testweb/cats/urls.py
+++ testweb/cats/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'cats'
+
 urlpatterns = [
     # /cats/
     url(r'^$', views.index, name='index'),
     # /cats/71
-    url(r'^(\d+)/$', views.detail, name='detail')
+    url(r'^(\d+)/$', views.detail, name='detail'),
+
+    # /cats/favorite
+    url(r'^(?P<kitten_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
 
 ]

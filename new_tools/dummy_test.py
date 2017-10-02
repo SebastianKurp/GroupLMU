1cbd1172f9590c55880149e3e8d5f54558892ca1
abdulzakkar <azakkar@luc.edu>
2017-10-01 20:03:43 -0500

    wrote a search function

---
+++ new_tools/dummy_test.py
@@ -0,0 +1,56 @@
+from search import search
+
+def main():
+	fileContents = ["""
+Dear Merrys:--As a subject appropriate to the season, I want to tell you about a New Year's breakfast which I had when I was a little girl. What do you think it was? A slice of dry bread and an apple. This is how it happened, and it is a true story, every word.
+As we came down to breakfast that morning, with very shiny faces and spandy clean aprons, we found father alone in the dining-room.
+"Happy New Year, papa! Where is mother?" we cried.
+"A little boy came begging and said they were starving at home, so your mother went to see and--ah, here she is."
+As papa spoke, in came mamma, looking very cold, rather sad, and very much excited.
+"Children, don't begin till you hear what I have to say," she cried; and we sat staring at her, with the breakfast untouched before us.
+"Not far away from here, lies a poor woman with a little new-born baby. Six children are huddled into one bed to keep from freezing, for they have no fire. There is nothing to eat over there; and the oldest boy came here to tell me they were starving this bitter cold day. My little girls, will you give them your breakfast, as a New Year's gift?"
+We sat silent a minute, and looked at the nice, hot porridge, creamy milk, and good bread and butter; for we were brought up like English children, and never drank tea or coffee, or ate anything but porridge for our breakfast.
+"I wish we'd eaten it up," thought I, for I was rather a selfish child, and very hungry.
+"I'm so glad you come before we began," said Nan, cheerfully.
+"May I go and help carry it to the poor, little children?" asked Beth, who had the tenderest heart that ever beat under a pinafore.
+"I can carry the lassy pot," said little May, proudly giving the thing she loved best.
+"And I shall take all the porridge," I burst in, heartily ashamed of my first feeling.
+"You shall put on your things and help me, and when we come back, we'll get something to eat," said mother, beginning to pile the bread and butter into a big basket.
+We were soon ready, and the procession set out. First, papa, with a basket of wood on one arm and coal on the other; mamma next, with a bundle of warm things and the teapot; Nan and I carried a pail of hot porridge between us, and each a pitcher of milk; Beth brought some cold meat, May the "lassy pot," and her old hood and boots; and Betsey, the girl, brought up the rear with a bag of potatoes and some meal.
+Fortunately it was early, and we went along back streets, so few people saw us, and no one laughed at the funny party.
+What a poor, bare, miserable place it was, to be sure,--broken windows, no fire, ragged clothes, wailing baby, sick mother, and a pile of pale, hungry children cuddled under one quilt, trying to keep warm. How the big eyes stared and the blue lips smiled as we came in!
+"Ah, mein Gott! it is the good angels that come to us!" cried the poor woman, with tears of joy.
+"Funny angels, in woollen hoods and red mittens," said I; and they all laughed.
+Then we fell to work, and in fifteen minutes, it really did seem as if fairies had been at work there. Papa made a splendid fire in the old fireplace and stopped up the broken window with his own hat and coat. Mamma set the shivering children round the fire, and wrapped the poor woman in warm things. Betsey and the rest of us spread the table, and fed the starving little ones.
+"Das ist gute!" "Oh, nice!" "Der angel--Kinder!" cried the poor things as they ate and smiled and basked in the warm blaze. We had never been called "angel-children" before, and we thought it very charming, especially I who had often been told I was "a regular Sancho." What fun it was! Papa, with a towel for an apron, fed the smallest child; mamma dressed the poor little new-born baby as tenderly as if it had been her own. Betsey gave the mother gruel and tea, and comforted her with assurance of better days for all. Nan, Lu, Beth, and May flew about among the seven children, talking and laughing and trying to understand their funny, broken English. It was a very happy breakfast, though we didn't get any of it; and when we came away, leaving them all so comfortable, and promising to bring clothes and food by and by, I think there were not in all the hungry little girls who gave away their breakfast, and contented themselves with a bit of bread and an apple of New Year's day.
+Testing the search for when, let's see
+	""",
+	"""
+Knowing that Mrs. Mallard was afflicted with a heart trouble, great care was taken to break to her as gently as possible the news of her husband's death.
+It was her sister Josephine who told her, in broken sentences; veiled hints that revealed in half concealing. Her husband's friend Richards was there, too, near her. It was he who had been in the newspaper office when intelligence of the railroad disaster was received, with Brently Mallard's name leading the list of "killed." He had only taken the time to assure himself of its truth by a second telegram, and had hastened to forestall any less careful, less tender friend in bearing the sad message.
+She did not hear the story as many women have heard the same, with a paralyzed inability to accept its significance. She wept at once, with sudden, wild abandonment, in her sister's arms. When the storm of grief had spent itself she went away to her room alone. She would have no one follow her.
+There stood, facing the open window, a comfortable, roomy armchair. Into this she sank, pressed down by a physical exhaustion that haunted her body and seemed to reach into her soul.
+She could see in the open square before her house the tops of trees that were all aquiver with the new spring life. The delicious breath of rain was in the air. In the street below a peddler was crying his wares. The notes of a distant song which someone was singing reached her faintly, and countless sparrows were twittering in the eaves.
+There were patches of blue sky showing here and there through the clouds that had met and piled one above the other in the west facing her window.
+She sat with her head thrown back upon the cushion of the chair, quite motionless, except when a sob came up into her throat and shook her, as a child who has cried itself to sleep continues to sob in its dreams.
+She was young, with a fair, calm face, whose lines bespoke repression and even a certain strength. But now there was a dull stare in her eyes, whose gaze was fixed away off yonder on one of those patches of blue sky. It was not a glance of reflection, but rather indicated a suspension of intelligent thought.
+There was something coming to her and she was waiting for it, fearfully. What was it? She did not know; it was too subtle and elusive to name. But she felt it, creeping out of the sky, reaching toward her through the sounds, the scents, the color that filled the air.
+Now her bosom rose and fell tumultuously. She was beginning to recognize this thing that was approaching to possess her, and she was striving to beat it back with her will--as powerless as her two white slender hands would have been. When she abandoned herself a little whispered word escaped her slightly parted lips. She said it over and over under the breath: "free, free, free!" The vacant stare and the look of terror that had followed it went from her eyes. They stayed keen and bright. Her pulses beat fast, and the coursing blood warmed and relaxed every inch of her body.
+She did not stop to ask if it were or were not a monstrous joy that held her. A clear and exalted perception enabled her to dismiss the suggestion as trivial. She knew that she would weep again when she saw the kind, tender hands folded in death; the face that had never looked save with love upon her, fixed and gray and dead. But she saw beyond that bitter moment a long procession of years to come that would belong to her absolutely. And she opened and spread her arms out to them in welcome.
+There would be no one to live for during those coming years; she would live for herself. There would be no powerful will bending hers in that blind persistence with which men and women believe they have a right to impose a private will upon a fellow-creature. A kind intention or a cruel intention made the act seem no less a crime as she looked upon it in that brief moment of illumination.
+And yet she had loved him--sometimes. Often she had not. What did it matter! What could love, the unsolved mystery, count for in the face of this possession of self-assertion which she suddenly recognized as the strongest impulse of her being!
+"Free! Body and soul free!" she kept whispering.
+Josephine was kneeling before the closed door with her lips to the keyhole, imploring for admission. "Louise, open the door! I beg; open the door--you will make yourself ill. What are you doing, Louise? For heaven's sake open the door."
+"Go away. I am not making myself ill." No; she was drinking in a very elixir of life through that open window.
+Her fancy was running riot along those days ahead of her. Spring days, and summer days, and all sorts of days that would be her own. She breathed a quick prayer that life might be long. It was only yesterday she had thought with a shudder that life might be long.
+She arose at length and opened the door to her sister's importunities. There was a feverish triumph in her eyes, and she carried herself unwittingly like a goddess of Victory. She clasped her sister's waist, and together they descended the stairs. Richards stood waiting for them at the bottom.
+Someone was opening the front door with a latchkey. It was Brently Mallard who entered, a little travel-stained, composedly carrying his grip-sack and umbrella. He had been far from the scene of the accident, and did not even know there had been one. He stood amazed at Josephine's piercing cry; at Richards' quick motion to screen him from the view of his wife.
+When the doctors came they said she had died of heart disease--of the joy that kills.
+	"""]
+	fileNames = ["Cousin Tribulation's Story", "The Story of An Hour"]
+	searchResults = search(fileContents,fileNames,"when",60)
+	for i in range(len(searchResults)):
+		for j in range(len(searchResults[i])):
+				print searchResults[i][j]
+
+main()
---
+++ new_tools/search.py
@@ -0,0 +1,83 @@
+def charsToSpaces(s):
+	output = ''
+	chars = '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~'
+	for i in range(len(s)):
+		if not s[i] in chars:
+			output += s[i]
+		else:
+			output += ' '
+	return output
+
+def search(fileContents,fileNames,term,maxOutputLen):
+	#file contents is a list of strings, each of which is the entire body of a note/file.
+	#file names is a list of strings as well, 
+		#only containing the name of the file which corresponds by index to the file contents list.
+	#term is just the search term
+	#max output length limits how long a string of output can be (for a clean output).
+	output = []
+	#final output
+	for i in range(len(fileNames)):
+		#iterate through each file
+		currFile = [fileNames[i]]
+		#start building a result for the current file, starting with this file's name
+		lines = fileContents[i].split('\n')
+		for j in range(len(lines)):
+			found = []
+			#list to compile all locations where it was found on the line
+			words = charsToSpaces(lines[j]).split(' ')
+			w = 0
+			for k in range(len(words)):
+				if words[k].lower() == term.lower():
+					found.append(w)
+					#if the word is found, append the index of the word
+				w += len(words[k]) + 1
+			for k in range(len(found)):
+				#for each location where the word was found, format the output
+				if len(lines[j]) <= maxOutputLen:
+					line = lines[j]
+					loc = found[k]
+				elif found[k] + len(term) + (maxOutputLen - len(term))/2 > len(lines[j]):
+					line = '... ' + lines[j][-(maxOutputLen-1):]
+					loc = maxOutputLen - (len(lines[j]) - found[k] + 1) + 4
+				elif found[k] - (maxOutputLen - len(term))/2 < 0:
+					line = lines[j][0:maxOutputLen] + ' ...'
+					loc = found[k]
+				else:
+					line = '... ' + lines[j][found[k] - (maxOutputLen - len(term))/2:found[k] + len(term) + (maxOutputLen - len(term))/2] + ' ...'
+					loc = (maxOutputLen + 8 - len(term))/2
+				currFile.append([line,j,loc,loc + len(term) - 1])
+				#append the formatted output to the current file's result
+		if len(currFile) > 1:
+			#if a word has been found in this file
+			output.append(currFile)
+			#add this file's results to the final output
+	return output
+
+### View sample input in 'dummy_test.py'
+
+### Format of search hits:
+#	[STRING of text surrounding the term, the line number, index of first letter of term in STRING, index of last letter of term in STRING]
+#		first and last letter indexes to help with highlighting the word
+
+### SAMPLE OUTPUT: search for 'when', with a max output length of 60
+"""
+
+[
+	["Cousin Tribulation's Story",
+		["... ear's breakfast which I had when I was a little girl. What d ...", 1, 32, 35],
+		["... our things and help me, and when we come back, we'll get som ...", 14, 32, 35],
+		["... e didn't get any of it; and when we came away, leaving them  ...", 21, 32, 35],
+		["Testing the search for when, let's see", 22, 23, 26]
+	],
+	["The Story of An Hour",
+		['... een in the newspaper office when intelligence of the railroa ...', 2, 32, 35],
+		["... ment, in her sister's arms. When the storm of grief had spen ...", 3, 32, 35],
+		['... r, quite motionless, except when a sob came up into her thro ...', 7, 32, 35],
+		['... nder hands would have been. When she abandoned herself a lit ...', 10, 32, 35],
+		['... w that she would weep again when she saw the kind, tender ha ...', 11, 32, 35],
+		['When the doctors came they said she had died of heart diseas ...', 20, 0, 3]
+	]
+]
+
+"""
+

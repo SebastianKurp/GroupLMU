2ae8c4cc9f8fabfa3702d58001e64c482db6f5ab
Paul Narup <pnarup@luc.edu>
2017-10-05 22:53:54 -0500

    unhighlighting words prevents the same word being logged twice. tested with js alert

--- testweb/notes/static/notes/highlight.js
+++ testweb/notes/static/notes/highlight.js
@@ -4,12 +4,21 @@ function getText(){
 
     text = (document.all) ? document.selection.createRange().text : window.getSelection().toString();
     text = text.trim();
+
     var length = text.length;
     if(length > 0 && text !=""){
         console.log(text);
+        unHighlight();
     }
-
 }
 
+function unHighlight() {
+    if ( document.selection ) {
+        document.selection.empty();
+    } else if ( window.getSelection ) {
+        window.getSelection().removeAllRanges();
+    }
+}
 document.onmouseup = getText;
+
 

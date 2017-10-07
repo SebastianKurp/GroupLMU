5ab37eefa18adc07a98bae01f940b5cad21c34fb
Paul Narup <pnarup@luc.edu>
2017-10-07 15:18:43 -0500

    Highlighting/unhighlighting functions are created. Currently throws error when highlighting is attempted on multiple lines. Also don't know when the unHighlighting should happen yet

--- testweb/notes/static/notes/highlight.js
+++ testweb/notes/static/notes/highlight.js
@@ -1,19 +1,38 @@
 
 function getText(){
     var text = "";
-
+    var textRange;
     text = (document.all) ? document.selection.createRange().text : window.getSelection().toString();
+
     text = text.trim();
 
+    textRange = window.getSelection().getRangeAt(0);//where to put the styling
+
+    highlight(textRange);
     var length = text.length;
     if(length > 0 && text !=""){
         console.log(text);
-        unHighlight();
+        //unHighlight(); //currently don't know how to implement this
+        deselect();
     }
 }
 
+function highlight(range){
+    var node = document.createElement("span");
+    node.setAttribute("style","background-color:yellow; display: inline;");
+    node.id = 'highlighted';
+    range.surroundContents(node);
+}
+
 function unHighlight() {
-    if ( document.selection ) {
+    var tag = document.getElementById('highlighted');
+    var content = document.getElementById('highlighted').textContent;
+    var node = document.createTextNode(content);
+    tag.parentNode.replaceChild(node,tag);
+}
+
+function deselect(){//deselects in browser to display the highlighting in yellow
+     if ( document.selection ) {
         document.selection.empty();
     } else if ( window.getSelection ) {
         window.getSelection().removeAllRanges();

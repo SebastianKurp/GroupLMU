2e92770e88b409c75306a6680abfa09a5c0de715
catemlitten <clitten@luc.edu>
2017-10-10 13:18:35 -0500

    Working with Abdul's code inside of custom tags

--- testweb/notes/templatetags/custom_tags.py
+++ testweb/notes/templatetags/custom_tags.py
@@ -1,8 +1,22 @@
 from django import template
-from ..models import note
+from .search import search
+from .report_by_symbol import reportBySymbol
+from .test_search import showResult
 
 register = template.Library()
 
 @register.filter(name='get_note_contents')
 def get_note_contents(value):
-    return value[:10]
+    return value[:10] + "..."
+
+# get_note_contents currently just
+# outputs a shortened version of the note's contents
+
+
+@register.filter(name='get_note')
+def get_note(note):
+    searchResults = search(note.content, note.title, "cat", 10)
+    print("Content: " + note.content + " Title: " +  note.title)
+    result = showResult("Testing...", searchResults)
+    print(searchResults)
+    return result
---
+++ testweb/notes/templatetags/report_by_symbol.py
@@ -0,0 +1,101 @@
+from .formatting import *
+import re
+
+def reportBySymbol(fileContents, fileNames, symbol, maxOutputLen = 60, style = 'by note'):
+    # file contents is a list of strings, each of which is the entire body of a note/file.
+    # file names is a list of strings as well,
+    # only containing the name of the file which corresponds by index to the file contents list.
+    # symbol is the STARTING CHARACTER that a word must have to be found.
+    # max output length limits how long a string of output can be (for a clean output).
+    #style: either 'by note' or 'by symbol'. Output for each style shown below.
+    output = []
+    # final output
+    for i in range(len(fileNames)):
+        # iterate through each file
+        currFile = [fileNames[i]]
+        # start building a result for the current file, starting with this file's name
+        lines = fileContents[i].split('\n')
+        for j in range(len(lines)):
+            found = []
+            mentions = []
+            words = (lines[j]).split(' ')
+            w = 0
+            for k in range(len(words)):
+                if re.search('^' + symbol, words[k]):
+                    found.append(w)
+                    mentions.append(words[k])
+                # if the word is a mention, append the index of the word
+                w += len(words[k]) + 1
+            for k in range(len(found)):
+                # for each location where the word was a mention, format the output
+                line = formatLine(lines[j], maxOutputLen, found[k], mentions[k], j)
+                currFile.append(line)
+                # append the formatted output to the current file's result
+        if len(currFile) > 1:
+            # if a mention in this file
+            output.append(currFile)
+            # add this file's results to the final output
+    if style == 'by symbol':
+        dSymbol = {}
+        for i in range(len(output)):
+            for j in range(1,len(output[i])):
+                word = output[i][j][0][output[i][j][2]:output[i][j][3]+1]
+                print(word)
+                if word in dSymbol.keys():
+                    dSymbol[word].append(output[i][0])
+                else:
+                    dSymbol[word] = [output[i][0]]
+        print(dSymbol)
+        tempOutput = []
+        for key, value in dSymbol.items():
+            tempOutput.append([key, list(set(value))])
+        output = tempOutput
+    return output
+
+### View sample input in 'dummy_test.py'
+
+### Format of symbol hits:
+#   [STRING of text surrounding the symbolized word, the line number, index of first letter of symbolized word in STRING,
+#   index of last letter of symbolized word in STRING]
+#       first and last letter indexes to help with highlighting the word
+
+### SAMPLE OUTPUT: search for '@' symbols, with a max output length of 60, with 'by note' style
+#       This means the results will be organized per note, and not per symbolized word
+"""
+
+[
+    ["The Story of An Hour",
+        ["... ently as possible the news of her husband's death. @mallard", 2, 55, 62]
+    ],
+    ["How the Camel Got His Hump",
+        ['... the Camel; and the Horse went away and told the Man. @camel', 5, 57, 62],
+        ['... aid the Camel; and the Dog went away and told the Man. @man', 7, 59, 62]
+    ],
+    ["The Cactus",
+        ['... agnified this exhibition of doubtful erudition. @carruthers', 12, 52, 62],
+        ["...  Trysdale's mind a swift @trysdale, scarifying retrospect of ...", 5, 29, 38]
+    ]
+]
+
+"""
+
+### SAMPLE OUTPUT: search for '#' symbols, with 'by symbol' style
+#       This means the results will be organized per symbolized word, and not per note
+"""
+
+[
+    ["#excited",
+        ['How the Camel Got His Hump', "Cousin Tribulation's Story"]
+    ],
+    ["#porridge",
+        ["Cousin Tribulation's Story"]
+    ],
+    ["#quiet",
+        ['The Story of An Hour']
+    ],
+    ["#medicine",
+        ['The Story of An Hour']
+    ]
+]
+
+"""

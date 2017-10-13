b81239cb3fdb619716f53178663d69b53396ae25
Paul Narup <pnarup@luc.edu>
2017-10-13 17:23:03 -0500

    fixed highlighting a bit. still having issues that would impact current idea about storage

--- testweb/notes/static/notes/highlight.js
+++ testweb/notes/static/notes/highlight.js
@@ -1,21 +1,67 @@
 
 function getText(){
     var text = "";
-    var textRange;
+    var textRange,sel;
+    var startRange = 0;
+    var endRange = 0;
+    var parentNode;
     text = (document.all) ? document.selection.createRange().text : window.getSelection().toString();
 
     text = text.trim();
-
-    textRange = window.getSelection().getRangeAt(0);//where to put the styling
-
-
     var length = text.length;
-    if(length > 0 && text !=""){
-        console.log(text);
-        highlight(textRange);
+    sel = window.getSelection().getRangeAt(0);
+    textRange = window.getSelection().getRangeAt(0);//where to put the styling
+    parentNode = textRange.startContainer.parentNode.id;
+
+
+
+
+    if(length > 0 && text !="" && parentNode =="content"){
+        startRange = textRange.startOffset;
+        endRange = textRange.endOffset;
+        if(sel.rangeCount && sel.getRangeAt){
+            textRange = sel.getRangeAt(0);
+        }
+
+        document.designMode = "on";
+
+
+
+        document.execCommand("BackColor", false,"#FFFF00");
+
+
+        document.designMode = "off";
+
+
         //unHighlight(); //currently don't know how to implement this
         deselect();
+        console.log(startRange);
+        console.log(endRange);
+
+
+    }else{
+        console.log(startRange);
+        console.log(endRange);
+        deselect();
     }
+}
+
+function newHighlight(){
+    var range, sel;
+
+    if(window.getSelection){
+        try{
+            if(!document.execCommand("BackColor", false, "#FFFF00")){
+                getText();
+            }
+        } catch(ex){
+            getText();
+        }
+    }else if(document.selection && document.selection.createRange){
+        range = document.selection.createRange();
+        range.execCommand("BackColor", false, "#FFFF00");
+    }
+
 }
 
 function highlight(range){
@@ -24,6 +70,16 @@ function highlight(range){
     node.id = 'highlighted';
     range.surroundContents(node);
 }
+
+
+
+/*function removeAllRanges(range){
+    if(range.rangeCount > 1){
+        for(var i = 1; i<range.rangeCount; i++){
+            range.removeRance(range.getRangeAt(i));
+        }
+    }
+}*/
 
 function unHighlight() {
     var tag = document.getElementById('highlighted');
@@ -41,6 +97,6 @@ function deselect(){//deselects in browser to display the highlighting in yellow
 }
 
 
-document.onmouseup = getText;
+document.onmouseup = newHighlight;
 
 

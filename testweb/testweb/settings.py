72bcd95a94a1fb4abd27c71ba931d2acb2dc81ef
catemlitten <clitten@luc.edu>
2017-10-14 13:01:45 -0500

    Heroku work

--- testweb/testweb/settings.py
+++ testweb/testweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['comp330.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['evercat.herokuapp.com', '127.0.0.1']
 #'*' would be a bad idea in actual production
 
 

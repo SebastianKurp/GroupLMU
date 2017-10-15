8bb962f03bb38e09268a03209e6de63009bd8369
catemlitten <clitten@luc.edu>
2017-10-14 19:41:45 -0500

    Broke a lot of stuff. Unbroke it.

--- testweb/notes/static/notes/highlight.js
+++ testweb/notes/static/notes/highlight.js
@@ -1,35 +1,56 @@
 
 function getText(){
     var text = "";
-    var textRange;
+    var textRange,sel;
+    var startRange = 0;
+    var endRange = 0;
+    var parentNode;
     text = (document.all) ? document.selection.createRange().text : window.getSelection().toString();
 
     text = text.trim();
-
-    textRange = window.getSelection().getRangeAt(0);//where to put the styling
-
-
     var length = text.length;
-    if(length > 0 && text !=""){
-        console.log(text);
-        highlight(textRange);
-        //unHighlight(); //currently don't know how to implement this
+    sel = window.getSelection().getRangeAt(0);
+    textRange = sel.cloneRange();//where to put the styling
+    parentNode = textRange.startContainer.parentNode.id;
+
+    if(length > 0 && text !="" && parentNode =="content"){//only highlights body of note
+        /*prevents offsets from referencing spans instead of the parent node*/
+        textRange.selectNodeContents(textRange.startContainer.parentNode);
+        textRange.setEnd(sel.startContainer, sel.startOffset);
+        startRange = textRange.toString().length;
+        endRange = startRange + sel.toString().length;
+
+        document.designMode = "on";//highlights stuff
+        document.execCommand("BackColor", false,"#FFFF00");
+        document.designMode = "off";
+
+        deselect();
+        console.log(startRange);
+        console.log(endRange);
+
+    }else{
+        console.log(startRange);
+        console.log(endRange);
         deselect();
     }
 }
 
-function highlight(range){
-    var node = document.createElement("span");
-    node.setAttribute("style","background-color:yellow; display: inline;");
-    node.id = 'highlighted';
-    range.surroundContents(node);
-}
+function highlight(){
+    var range, sel;
 
-function unHighlight() {
-    var tag = document.getElementById('highlighted');
-    var content = document.getElementById('highlighted').textContent;
-    var node = document.createTextNode(content);
-    tag.parentNode.replaceChild(node,tag);
+    if(window.getSelection){
+        try{
+            if(!document.execCommand("BackColor", false, "#FFFF00")){
+                getText();
+            }
+        } catch(ex){
+            getText();
+        }
+    }else if(document.selection && document.selection.createRange){
+        range = document.selection.createRange();
+        range.execCommand("BackColor", false, "#FFFF00");
+    }
+
 }
 
 function deselect(){//deselects in browser to display the highlighting in yellow
@@ -39,5 +60,7 @@ function deselect(){//deselects in browser to display the highlighting in yellow
         window.getSelection().removeAllRanges();
     }
 }
-document.onmouseup = getText;
+
+
+document.onmouseup = highlight;
 
